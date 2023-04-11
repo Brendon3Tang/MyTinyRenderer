fd7bb9fab166e5f79bc298296d001c9716e98530
brendon3tang <kyle3tang@gmail.com>
2023-04-11 20:12:42 +0800

    lesson 2

--- main.cpp
+++ main.cpp
@@ -90,37 +90,164 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 }
 
+/*
+// Old-school method: Line sweeping：
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
+    if (t0.y==t1.y && t0.y==t2.y) return;
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+    if (t0.y>t1.y) std::swap(t0, t1); 
+    if (t0.y>t2.y) std::swap(t0, t2); 
+    if (t1.y>t2.y) std::swap(t1, t2); 
+    int total_height = t2.y-t0.y; 
+    // for (int y=t0.y; y<=t1.y; y++) { 
+    //     int segment_height = t1.y-t0.y+1; 
+    //     float alpha = (float)(y-t0.y)/total_height; 
+    //     float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero 
+    //     Vec2i A = t0 + (t2-t0)*alpha; 
+    //     Vec2i B = t0 + (t1-t0)*beta; 
+    //     if (A.x>B.x) std::swap(A, B); 
+    //     for (int j=A.x; j<=B.x; j++) { 
+    //         image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+    //     } 
+    // } 
+    // for (int y=t1.y; y<=t2.y; y++) { 
+    //     int segment_height =  t2.y-t1.y+1; 
+    //     float alpha = (float)(y-t0.y)/total_height; 
+    //     float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero 
+    //     Vec2i A = t0 + (t2-t0)*alpha; 
+    //     Vec2i B = t1 + (t2-t1)*beta; 
+    //     if (A.x>B.x) std::swap(A, B); 
+    //     for (int j=A.x; j<=B.x; j++) { 
+    //         image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+    //     } 
+    // }
+
+    //整合一下，用一个for loop一起画
+    for(int y = t0.y; y < t2.y; y++){
+        bool second_half = y > t1.y || (t1.y == t0.y);
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)(y - t0.y)/total_height;
+        float beta  = (float)(y-(second_half ? t1.y : t0.y))/segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+        if(A.x > B.x)   swap(A, B);
+        for(int j = A.x; j <= B.x; j++){
+            image.set(j, y, color);
+        }
+    } 
+}
+*/
+
+//new method: 用 barycentric画:
+bool barycentric(Vec2i *pts, Vec2i P){
+    Vec2i A = pts[0];
+    Vec2i B = pts[1];
+    Vec2i C = pts[2];
+    float alpha = (-((float)P.x - B.x)*(C.y - B.y) + (P.y - B.y)*(C.x - B.x)) / 
+    (-(A.x - B.x)*(C.y - B.y) + (A.y - B.y)*(C.x - B.x));
+
+    float beta = (-((float)P.x - C.x)*(A.y - C.y) + (P.y - C.y)*(A.x - C.x)) / 
+    (-(B.x - C.x)*(A.y - C.y) + (B.y - C.y)*(A.x - C.x));
+
+    float lambda = 1.0f - alpha - beta;
+
+    if(alpha >= 0.0f && beta >= 0.f && lambda >= 0.f && alpha <= 1.0f && beta <= 1.f && lambda <= 1.f) 
+        return true;
+
+    return false;
+}
+
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color){
+    //barycentric(pts);
+    Vec2i miniBound(image.get_width()-1, image.get_height()-1);
+    Vec2i maxBound(0,0);
+
+    for(int i = 0; i < 3; i++){
+        miniBound.x = max(0, min(miniBound.x, pts[i].x));
+        miniBound.y = max(0, min(miniBound.y, pts[i].y));
+
+        maxBound.x = min(image.get_width()-1, max(maxBound.x, pts[i].x));
+        maxBound.y = min(image.get_height()-1, max(maxBound.y, pts[i].y));
+    }
+
+    Vec2i P;
+    for(int i = miniBound.x; i <= maxBound.x; i++){
+        for(int j = miniBound.y; j <= maxBound.y; j++){
+            P = Vec2i(i, j);
+            // Vec3f baryC = barycentric(pts, P);
+            // if(baryC.x < 0 || baryC.y < 0 || baryC.z < 0) continue;
+            if(barycentric(pts, P))
+                image.set(P.x, P.y, color);
+        }
+    }
+}
+
 int main(int argc, char** argv){
-    TGAImage image(Width, Height, TGAImage::RGB);
+    // TGAImage image(Width, Height, TGAImage::RGB);
+    //lesson 0
     // line(13, 20, 80, 40, image, white); 
     // line(10, 10, 70, 70, image, white); 
     // line(20, 13, 40, 80, image, red); 
     // line(80, 40, 13, 20, image, red);
 
+    //lesson 1
     //载入object，如果command line有指定object则载入指定object，否则载入african_head
+    // if(argc == 2)
+    //     model = new Model(argv[1]);
+    // else
+    //     model = new Model(fileName);
+    
+    // for(int i = 0; i < model->nfaces(); i++){
+    //     vector<int> face = model->face(i);//取得object的其中一个face。每个face由三个顶点组成。
+    //     for(int j = 0; j < 3; j++){//取得三个顶点中的两个，并且把他们连起来
+    //     //三次loop中分别找到当前的三个边(vertex0，vertex1)，(vertex1，vertex2)，(vertex2，vertex0)，然后把线画出来
+    //         Vec3f v0 = model->vert(face[j]);    
+    //         Vec3f v1 = model->vert(face[(j+1)%3]);
+    //         //把模型点移动到image的中心：
+    //         //  1. 定点坐标全部+1是为了把模型的中心的坐标从(0,0)移动到(1，1)。不移动的话模型的中心会在左下角，我们只能看到四分之一
+    //         //  2. 为了让模型的大小适应image所以是把x，y坐标全部乘Width/2与Height/2。不乘的话模型会很小，只在一个pixel里显现
+    //         int x0 = (v0.x + 1) * Width/2;    
+    //         int y0 = (v0.y + 1) * Height/2;
+    //         int x1 = (v1.x + 1) * Width/2;    
+    //         int y1 = (v1.y + 1) * Height/2;
+    //         line(x0, y0, x1, y1, image, green);
+    //     }
+    // }
+
+    //lesson 2
+    // old way画三角形
+    // Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+    // Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+    // triangle(t0[0], t0[1], t0[2], image, red); 
+    // triangle(t1[0], t1[1], t1[2], image, white); 
+    // triangle(t2[0], t2[1], t2[2], image, green);
+    
+    //barycentric画三角形
+    // TGAImage image(200, 200, TGAImage::RGB);
+    // Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
+    // triangle(pts, image, TGAColor(255, 0, 0, 255));
+
+    TGAImage image(Width, Height, TGAImage::RGB);
     if(argc == 2)
         model = new Model(argv[1]);
     else
         model = new Model(fileName);
-    
     for(int i = 0; i < model->nfaces(); i++){
-        vector<int> face = model->face(i);//取得object的其中一个三角形
-        for(int j = 0; j < 3; j++){//遍历这个三角形，
-        //找到当前三角形的(vertex0，vertex1)，(vertex1，vertex2)，(vertex2，vertex0)，然后把线画出来
-            Vec3f v0 = model->vert(face[j]);    
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            //把模型点移动到image的中心：
-            //  1. 定点坐标全部+1是为了把模型的中心的坐标从(0,0)移动到(1，1)。不移动的话模型的中心会在左下角，我们只能看到四分之一
-            //  2. 为了让模型的大小适应image所以是把x，y坐标全部乘Width/2与Height/2。不乘的话模型会很小，只在一个pixel里显现
-            int x0 = (v0.x + 1) * Width/2;    
-            int y0 = (v0.y + 1) * Height/2;
-            int x1 = (v1.x + 1) * Width/2;    
-            int y1 = (v1.y + 1) * Height/2;
-            line(x0, y0, x1, y1, image, green);
+        vector<int> face = model->face(i); 
+        Vec2i screenCoord[3];
+        for(int j = 0; j < 3; j++){
+            Vec3f worldCoord = model->vert(face[j]);
+            screenCoord[j] = Vec2i((worldCoord.x + 1) * Width/2., (worldCoord.y + 1) * Height/2.);
         }
+        triangle(screenCoord, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
+
+    // flat shader
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
+    image.write_tga_file("outputFlatShader.tga");
     delete model;
     return 0;
 }
