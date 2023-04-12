fa41e295a0fce5fac4b998a9be114dffb5b82510
brendon3tang <kyle3tang@gmail.com>
2023-04-12 23:37:07 +0800

    learning IO & texture mapping of C++ in model.cpp

--- main.cpp
+++ main.cpp
@@ -1,98 +1,239 @@
-/**
- * Bresenham’s Line Drawing Algorithm
-*/
-#include "tgaimage.h"
-#include "geometry.h"
-#include "model.h"
-#include <iostream>
+// /**
+//  * Bresenham’s Line Drawing Algorithm
+// */
+// #include "tgaimage.h"
+// #include "geometry.h"
+// #include "model.h"
+// #include <iostream>
+// #include <vector>
+// #include <cmath>
+// using namespace std;
+
+// const TGAColor white = TGAColor(255, 255, 255, 255);
+// const TGAColor red   = TGAColor(255, 0,   0,   255);
+// const TGAColor green = TGAColor(0,   255, 128, 255);
+// Model* model = nullptr;
+// const int Height = 800;
+// const int Width  = 800;
+// const char *fileName = "obj/african_head.obj";
+
+// //new method: 用 barycentric画:
+// // barycentric会用重心坐标判断点P是否在由pts0,pts1,pts2三个顶点组成的三角形内，如果是就返回true，否则返回false
+// Vec3f barycentric(Vec3f *pts, Vec3f P){
+//     Vec3f A = pts[0];
+//     Vec3f B = pts[1];
+//     Vec3f C = pts[2];
+//     float alpha = (-((float)P.x - B.x)*(C.y - B.y) + (P.y - B.y)*(C.x - B.x)) / 
+//     (-(A.x - B.x)*(C.y - B.y) + (A.y - B.y)*(C.x - B.x));
+
+//     float beta = (-((float)P.x - C.x)*(A.y - C.y) + (P.y - C.y)*(A.x - C.x)) / 
+//     (-(B.x - C.x)*(A.y - C.y) + (B.y - C.y)*(A.x - C.x));
+
+//     float gamma = 1.0f - alpha - beta;
+
+//     return Vec3f(alpha, beta, gamma);
+// }
+
+// void triangle(Vec3f *pts, float **zbuffer, TGAImage &image, TGAColor color){
+//     //对于每个三角形，我们会在屏幕内找到最小的能够把它包裹起来的box，然后在这个box内部进行shading（提高效率）
+//     Vec2f miniBound(image.get_width()-1, image.get_height()-1);
+//     Vec2f maxBound(0,0);
+
+//     //遍历三角形的三个顶点，找到每个三角形的最高点与最低点，最左点与最右点，这个三角形一定在这个box内
+//     for(int i = 0; i < 3; i++){
+//         miniBound.x = max(0.f, min(miniBound.x, pts[i].x));
+//         miniBound.y = max(0.f, min(miniBound.y, pts[i].y));
+
+//         maxBound.x = min((float)image.get_width()-1, max(maxBound.x, pts[i].x));
+//         maxBound.y = min((float)image.get_height()-1, max(maxBound.y, pts[i].y));
+//     }
+
+//     //定义点P。P可以是box内的任意一个点，我们需要逐个遍历，如果P刚好在三角形内部，那么我们就把这个三角形着色shading
+//     Vec3f P;
+//     for(int i = miniBound.x; i <= maxBound.x; i++){
+//         for(int j = miniBound.y; j <= maxBound.y; j++){
+//             P = Vec3f(i, j, 0);    //取得当前点P
+//             Vec3f baryCentricCoord = barycentric(pts, P);
+//             //求出重心坐标alpha, beta, lambda。当且仅当这三个值都大于0时，点才会在三角形平面内
+//             if(baryCentricCoord.x >= 0.0f && baryCentricCoord.y >= 0.f && baryCentricCoord.z >= 0.f){  //如果当前点P在三角形内，就shading
+//                 for(int k = 0; k < 3; k++)  P.z += pts[k].z * baryCentricCoord[k];
+//                 if (zbuffer[(int)P.x][(int)P.y]<P.z) {
+//                     zbuffer[(int)P.x][(int)P.y] = P.z;
+//                     image.set(P.x, P.y, color);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// int main(int argc, char** argv){
+//     //flat shader + simple shadow
+//     TGAImage image(Width, Height, TGAImage::RGB);
+//     if(argc == 2)
+//         model = new Model(argv[1]);
+//     else
+//         model = new Model(fileName);
+//     Vec3f light_dir(0., 0., -1.);   //定义光源的位置（用于shadowing）
+//     float **zbuffer = new float*[Width];
+//     for(int i = 0; i < Width; i++){
+//         zbuffer[i] = new float[Height];
+//         for(int j = 0; j < Height; j++){
+//             zbuffer[i][j] = -std::numeric_limits<float>::max();
+//         }
+//     }
+
+//     //遍历所有的面，每个face都是一个三角形，有三个顶点vertex，每个vertex有(x,y,z)三个坐标
+//     for(int i = 0; i < model->nfaces(); i++){
+//         vector<int> face = model->face(i); //用vector取出每个face，方便后续操作
+//         Vec3f screenCoord[3];   //保存三角形三个vertex在屏幕上出现的位置
+//         Vec3f worldCoord[3];    //保存三角形三个vertex的实际坐标
+//         //遍历三个顶点
+//         for(int j = 0; j < 3; j++){
+//             worldCoord[j] = model->vert(face[j]);   //用vert()取出该vertex的坐标(x,y)并保存在worldCoord中
+//             screenCoord[j] = Vec3f(int((worldCoord[j].x + 1) * Width/2. + .5), int((worldCoord[j].y + 1) * Height/2. + .5), worldCoord[j].z);    //转换到屏幕上的位置
+//         }
+//         // 求出三角形平面的法线。
+//         Vec3f n = cross(Vec3f(worldCoord[2] - worldCoord[0]), Vec3f(worldCoord[1] - worldCoord[0]));
+//         n = n.normalize();
+//         // 穿过三角形平面的光的intensity与光源和平面法线的夹脚有关，当光来源与平面平行时，基本没有光穿过平面，可以不着色（阴影）。因此我们此处用点乘
+//         float intensity = n * light_dir;
+//         if(intensity > 0)   //只有有光穿过平面时我们才会着色，且颜色深度与intensity有关。
+//             triangle(screenCoord, zbuffer, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+//     }
+
+//     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+//     image.write_tga_file("outputZBuffer.tga");
+//     delete model;
+//     delete []zbuffer;
+//     return 0;
+// }
+
 #include <vector>
 #include <cmath>
-using namespace std;
+#include <limits>
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
-const TGAColor green = TGAColor(0,   255, 128, 255);
-Model* model = nullptr;
-const int Height = 800;
-const int Width  = 800;
-const char *fileName = "obj/african_head.obj";
+const int width  = 800;
+const int height = 800;
+const int depth  = 255;
 
-//new method: 用 barycentric画:
-// barycentric会用重心坐标判断点P是否在由pts0,pts1,pts2三个顶点组成的三角形内，如果是就返回true，否则返回false
-bool barycentric(Vec2i *pts, Vec2i P){
-    Vec2i A = pts[0];
-    Vec2i B = pts[1];
-    Vec2i C = pts[2];
-    float alpha = (-((float)P.x - B.x)*(C.y - B.y) + (P.y - B.y)*(C.x - B.x)) / 
-    (-(A.x - B.x)*(C.y - B.y) + (A.y - B.y)*(C.x - B.x));
+Model *model = NULL;
+int *zbuffer = NULL;
+Vec3f light_dir(0,0,-1);
+Vec3f camera(0,0,3);
 
-    float beta = (-((float)P.x - C.x)*(A.y - C.y) + (P.y - C.y)*(A.x - C.x)) / 
-    (-(B.x - C.x)*(A.y - C.y) + (B.y - C.y)*(A.x - C.x));
-
-    float lambda = 1.0f - alpha - beta;
-    //求出重心坐标alpha, beta, lambda。当且仅当这三个值都大于0时，点才会在三角形平面内
-    if(alpha >= 0.0f && beta >= 0.f && lambda >= 0.f) 
-        return true;
-
-    return false;
+Vec3f m2v(Matrix m) {
+    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
 }
 
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color){
-    //对于每个三角形，我们会在屏幕内找到最小的能够把它包裹起来的box，然后在这个box内部进行shading（提高效率）
-    Vec2i miniBound(image.get_width()-1, image.get_height()-1);
-    Vec2i maxBound(0,0);
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
 
-    //遍历三角形的三个顶点，找到每个三角形的最高点与最低点，最左点与最右点，这个三角形一定在这个box内
-    for(int i = 0; i < 3; i++){
-        miniBound.x = max(0, min(miniBound.x, pts[i].x));
-        miniBound.y = max(0, min(miniBound.y, pts[i].y));
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
 
-        maxBound.x = min(image.get_width()-1, max(maxBound.x, pts[i].x));
-        maxBound.y = min(image.get_height()-1, max(maxBound.y, pts[i].y));
-    }
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
 
-    //定义点P。P可以是box内的任意一个点，我们需要逐个遍历，如果P刚好在三角形内部，那么我们就把这个三角形着色shading
-    Vec2i P;
-    for(int i = miniBound.x; i <= maxBound.x; i++){
-        for(int j = miniBound.y; j <= maxBound.y; j++){
-            P = Vec2i(i, j);    //取得当前点P
-            if(barycentric(pts, P)) //如果当前点P在三角形内，就shading
-                image.set(P.x, P.y, color);
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, TGAImage &image, float intensity, int *zbuffer) {
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+    if (t0.y>t1.y) { std::swap(t0, t1); std::swap(uv0, uv1); }
+    if (t0.y>t2.y) { std::swap(t0, t2); std::swap(uv0, uv2); }
+    if (t1.y>t2.y) { std::swap(t1, t2); std::swap(uv1, uv2); }
+
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+        Vec3i A   =               t0  + Vec3f(t2-t0  )*alpha;
+        Vec3i B   = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
+        Vec2i uvA =               uv0 +      (uv2-uv0)*alpha;
+        Vec2i uvB = second_half ? uv1 +      (uv2-uv1)*beta : uv0 +      (uv1-uv0)*beta;
+        if (A.x>B.x) { std::swap(A, B); std::swap(uvA, uvB); }
+        for (int j=A.x; j<=B.x; j++) {
+            float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+            Vec3i   P = Vec3f(A) + Vec3f(B-A)*phi;
+            Vec2i uvP =     uvA +   (uvB-uvA)*phi;
+            int idx = P.x+P.y*width;
+            if (zbuffer[idx]<P.z) {
+                zbuffer[idx] = P.z;
+                TGAColor color = model->diffuse(uvP);
+                image.set(P.x, P.y, TGAColor(color.r*intensity, color.g*intensity, color.b*intensity));
+            }
         }
     }
 }
 
-int main(int argc, char** argv){
-    //flat shader + simple shadow
-    TGAImage image(Width, Height, TGAImage::RGB);
-    if(argc == 2)
+int main(int argc, char** argv) {
+    if (2==argc) {
         model = new Model(argv[1]);
-    else
-        model = new Model(fileName);
-    Vec3f light_dir(0., 0., -1.);   //定义光源的位置（用于shadowing）
-
-    //遍历所有的面，每个face都是一个三角形，有三个顶点vertex，每个vertex有(x,y,z)三个坐标
-    for(int i = 0; i < model->nfaces(); i++){
-        vector<int> face = model->face(i); //用vector取出每个face，方便后续操作
-        Vec2i screenCoord[3];   //保存三角形三个vertex在屏幕上出现的位置
-        Vec3f worldCoord[3];    //保存三角形三个vertex的实际坐标
-        //遍历三个顶点
-        for(int j = 0; j < 3; j++){
-            worldCoord[j] = model->vert(face[j]);   //用vert()取出该vertex的坐标(x,y)并保存在worldCoord中
-            screenCoord[j] = Vec2i((worldCoord[j].x + 1) * Width/2., (worldCoord[j].y + 1) * Height/2.);    //转换到屏幕上的位置
-        }
-
-        // 求出三角形平面的法线。
-        Vec3f n = Vec3f(worldCoord[2] - worldCoord[0]) ^ Vec3f(worldCoord[1] - worldCoord[0]);
-        n = n.normalize();
-        // 穿过三角形平面的光的intensity与光源和平面法线的夹脚有关，当光来源与平面平行时，基本没有光穿过平面，可以不着色（阴影）。因此我们此处用点乘
-        float intensity = n * light_dir;
-        if(intensity > 0)   //只有有光穿过平面时我们才会着色，且颜色深度与intensity有关。
-            triangle(screenCoord, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+    } else {
+        model = new Model("obj/african_head.obj");
     }
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("outputFlatShaderShadow.tga");
+    zbuffer = new int[width*height];
+    for (int i=0; i<width*height; i++) {
+        zbuffer[i] = std::numeric_limits<int>::min();
+    }
+
+    { // draw the model
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+        Projection[3][2] = -1.f/camera.z;
+
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i=0; i<model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            for (int j=0; j<3; j++) {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] =  m2v(ViewPort*Projection*v2m(v));
+                world_coords[j]  = v;
+            }
+            Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+            n.normalize();
+            float intensity = n*light_dir;
+            if (intensity>0) {
+                Vec2i uv[3];
+                for (int k=0; k<3; k++) {
+                    uv[k] = model->uv(i, k);
+                }
+                triangle(screen_coords[0], screen_coords[1], screen_coords[2], uv[0], uv[1], uv[2], image, intensity, zbuffer);
+            }
+        }
+
+        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        image.write_tga_file("outputTexture.tga");
+    }
+
+    { // dump z-buffer (debugging purposes only)
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i=0; i<width; i++) {
+            for (int j=0; j<height; j++) {
+                zbimage.set(i, j, TGAColor(zbuffer[i+j*width], 1));
+            }
+        }
+        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        zbimage.write_tga_file("zbuffer.tga");
+    }
     delete model;
+    delete [] zbuffer;
     return 0;
 }
--- model.cpp
+++ model.cpp
@@ -1,3 +1,58 @@
+// #include <iostream>
+// #include <string>
+// #include <fstream>
+// #include <sstream>
+// #include <vector>
+// #include "model.h"
+
+// Model::Model(const char *filename) : verts_(), faces_() {
+//     std::ifstream in;
+//     in.open (filename, std::ifstream::in);
+//     if (in.fail()) return;
+//     std::string line;
+//     while (!in.eof()) {
+//         std::getline(in, line);
+//         std::istringstream iss(line.c_str());
+//         char trash;
+//         if (!line.compare(0, 2, "v ")) {
+//             iss >> trash;
+//             Vec3f v;
+//             for (int i=0;i<3;i++) iss >> v[i];
+//             verts_.push_back(v);
+//         } else if (!line.compare(0, 2, "f ")) {
+//             std::vector<int> f;
+//             int itrash, idx;
+//             iss >> trash;
+//             while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+//                 idx--; // in wavefront obj all indices start at 1, not zero
+//                 f.push_back(idx);
+//             }
+//             faces_.push_back(f);
+//         }
+//     }
+//     std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+// }
+
+// Model::~Model() {
+// }
+
+// int Model::nverts() {
+//     return (int)verts_.size();
+// }
+
+// int Model::nfaces() {
+//     return (int)faces_.size();
+// }
+
+// std::vector<int> Model::face(int idx) {
+//     return faces_[idx];
+// }
+
+// Vec3f Model::vert(int i) {
+//     return verts_[i];
+// }
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,32 +60,46 @@
 #include <vector>
 #include "model.h"
 
-Model::Model(const char *filename) : verts_(), faces_() {
+Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_() {
     std::ifstream in;
     in.open (filename, std::ifstream::in);
     if (in.fail()) return;
     std::string line;
     while (!in.eof()) {
-        std::getline(in, line);
-        std::istringstream iss(line.c_str());
+        std::getline(in, line); //把file里的data全部放入string line里。
+        std::istringstream iss(line.c_str());   //line.c_str()它返回当前字符串的首字符地址。
+                                                //换种说法，c_str()函数返回一个指向正规C字符串的指针常量，
+                                                //内容与本string串相同。这是为了与C语言兼容，在C语言中没有string类型，
+                                                //故必须通过string类对象的成员函数c_str()把string对象转换成C中的字符串样式。
         char trash;
-        if (!line.compare(0, 2, "v ")) {
-            iss >> trash;
+        if (!line.compare(0, 2, "v ")) {    //compare(line的起始位置start，line中要被比较的子字符串的长度 i:[start, i-1], 与line比较的字符)
+            iss >> trash;                   //compare 返回0如果相等。
             Vec3f v;
-            for (int i=0;i<3;i++) iss >> v.raw[i];
+            for (int i=0;i<3;i++) iss >> v[i];  // 每个顶点v 由（x,y,z）三个元素组成。
             verts_.push_back(v);
-        } else if (!line.compare(0, 2, "f ")) {
-            std::vector<int> f;
-            int itrash, idx;
-            iss >> trash;
-            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--; // in wavefront obj all indices start at 1, not zero
-                f.push_back(idx);
+        } else if (!line.compare(0, 3, "vn ")) {
+            iss >> trash >> trash;  //把'v','n'先放入trash，然后再把有用的数据放入n[i]。空格是分隔符不需要放入trash，会被自动忽略，所以只需要两次放入trash
+            Vec3f n;
+            for (int i=0;i<3;i++) iss >> n[i];  // 每个法线Vn 由（x,y,z）三个元素组成。
+            norms_.push_back(n);
+        } else if (!line.compare(0, 3, "vt ")) {
+            iss >> trash >> trash;              //把'v','t'先放入trash，然后再把有用的数据放入uv[i]。
+            Vec2f uv;
+            for (int i=0;i<2;i++) iss >> uv[i]; // 每个纹理坐标Vt 由（u,v）两个元素组成。
+            uv_.push_back(uv);
+        }  else if (!line.compare(0, 2, "f ")) {
+            std::vector<Vec3i> f;
+            Vec3i tmp;
+            iss >> trash;   //先把'f'放入trash
+            while (iss >> tmp[0] >> trash >> tmp[1] >> trash >> tmp[2]) {//对于f:283/266/283，把数字放入tmp，把'/'放入trash
+                for (int i=0; i<3; i++) tmp[i]--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(tmp);
             }
             faces_.push_back(f);
         }
     }
-    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << " vt# " << uv_.size() << " vn# " << norms_.size() << std::endl;
+    load_texture(filename, "_diffuse.tga", diffusemap_);
 }
 
 Model::~Model() {
@@ -45,10 +114,31 @@ int Model::nfaces() {
 }
 
 std::vector<int> Model::face(int idx) {
-    return faces_[idx];
+    std::vector<int> face;
+    for (int i=0; i<(int)faces_[idx].size(); i++) face.push_back(faces_[idx][i][0]);
+    return face;
 }
 
 Vec3f Model::vert(int i) {
     return verts_[i];
+}
+
+void Model::load_texture(std::string filename, const char *suffix, TGAImage &img) {
+    std::string texfile(filename);
+    size_t dot = texfile.find_last_of(".");
+    if (dot!=std::string::npos) {
+        texfile = texfile.substr(0,dot) + std::string(suffix);
+        std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+        img.flip_vertically();
+    }
+}
+
+TGAColor Model::diffuse(Vec2i uv) {
+    return diffusemap_.get(uv.x, uv.y);
+}
+
+Vec2i Model::uv(int iface, int nvert) {
+    int idx = faces_[iface][nvert][1];
+    return Vec2i(uv_[idx].x*diffusemap_.get_width(), uv_[idx].y*diffusemap_.get_height());
 }
 
