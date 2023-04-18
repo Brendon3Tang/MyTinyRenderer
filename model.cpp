4d2be713ea4396595d3178b01c75ef2c23da0c1f
brendon3tang <kyle3tang@gmail.com>
2023-04-18 10:55:55 +0800

    add funcs to load normal & specular texture

--- model.cpp
+++ model.cpp
@@ -45,6 +45,8 @@ Model::Model(const char *filename) : verts_(), faces_(), norms_(), uv_() {
     }
     std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << " vt# " << uv_.size() << " vn# " << norms_.size() << std::endl;
     load_texture(filename, "_diffuse.tga", diffusemap_);
+    load_texture(filename, "_nm.tga",      normalmap_);
+    load_texture(filename, "_spec.tga",    specularmap_);
 }
 
 Model::~Model() {
@@ -68,6 +70,10 @@ Vec3f Model::vert(int i) {
     return verts_[i];
 }
 
+Vec3f Model::vert(int iface, int nthvert) {
+    return verts_[faces_[iface][nthvert][0]];
+}
+
 void Model::load_texture(std::string filename, const char *suffix, TGAImage &img) {
     std::string texfile(filename);
     size_t dot = texfile.find_last_of(".");
@@ -78,17 +84,30 @@ void Model::load_texture(std::string filename, const char *suffix, TGAImage &img
     }
 }
 
-TGAColor Model::diffuse(Vec2i uv) {
-    return diffusemap_.get(uv.x, uv.y); //diffusemap_就是有纹理图的TGAImage。得到纹理图中(u,v)点的颜色数据并返回
+TGAColor Model::diffuse(Vec2f uvf) {
+    Vec2i uv(uvf[0]*diffusemap_.get_width(), uvf[1]*diffusemap_.get_height());
+    return diffusemap_.get(uv[0], uv[1]);//diffusemap_就是有纹理图的TGAImage。得到纹理图中(u,v)点的颜色数据并返回
 }
 
-// uv(int iface, int nvert)会对于当前face的当前vertex
-Vec2i Model::uv(int iface, int nvert) {
-    int idx = faces_[iface][nvert][1];  //取得当前face中当前vertex的纹理坐标的索引值(v/vt/vn中的vt，所以index是1)
-    return Vec2i(uv_[idx].x*diffusemap_.get_width(), uv_[idx].y*diffusemap_.get_height());
+Vec3f Model::normal(Vec2f uvf) {
+    Vec2i uv(uvf[0]*normalmap_.get_width(), uvf[1]*normalmap_.get_height());
+    TGAColor c = normalmap_.get(uv[0], uv[1]);
+    Vec3f res;
+    for (int i=0; i<3; i++)
+        res[2-i] = (float)c[i]/255.f*2.f - 1.f;
+    return res;
 }
 
-Vec3f Model::norm(int iface, int nvert) {
+float Model::specular(Vec2f uvf) {
+    Vec2i uv(uvf[0]*specularmap_.get_width(), uvf[1]*specularmap_.get_height());
+    return specularmap_.get(uv[0], uv[1])[0]/1.f;
+}
+
+Vec2f Model::uv(int iface, int nthvert) {
+    return uv_[faces_[iface][nthvert][1]];//faces_[iface][nthvert][1]:取得当前face中当前vertex的纹理坐标的索引值(v/vt/vn中的vt，所以index是1)
+}
+
+Vec3f Model::normal(int iface, int nvert) {
     int idx = faces_[iface][nvert][2];
     return norms_[idx].normalize();
 }
